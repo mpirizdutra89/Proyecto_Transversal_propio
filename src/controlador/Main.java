1be37c06d0be367e803534db9dfc0058d289dc4f
Martin Piriz <mpirizdutra@ulp.edu.ar>
2024-05-16 16:38:27 -0300

    armado del proyuecto

---
+++ src/controlador/Main.java
@@ -0,0 +1,23 @@
+package controlador;
+
+import accesoADatos.Conexion;
+import accesoADatos.AlumnoData;
+import entidades.Alumno;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Preubas para la etapa 1
+         //Alumno alumnos1=new Alumno(152659,"piriz","martin",LocalDate.of(1990, 12, 20),false);
+         //AlumnoData.guardarAlumno(alumnos1);
+    }
+    
+}
