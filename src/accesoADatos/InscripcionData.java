fb71016ba5f2bafe6fb205259e33218ba4c194e2
mpirizdutra89 <mpirizdutra@ulp.edu.ar>
2024-05-20 10:48:30 -0300

    inscripocion data

--- src/accesoADatos/InscripcionData.java
+++ src/accesoADatos/InscripcionData.java
@@ -50,11 +50,13 @@ public class InscripcionData {
                 res = true;
             }
             ps.close();
+
         } catch (SQLException ex) {
-            Conexion.msjError.add("IncripcionesData: guardarInscripcion ->" + ex.getMessage());
+            Conexion.msjError.add("IncripcionData: guardarInscripcion ->" + ex.getMessage());
         }
 
-        //ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce cierra un jFrame interno
+        // ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce
+        // cierra un jFrame interno
         return res;
     }
 
@@ -63,13 +65,13 @@ public class InscripcionData {
         ResultSet res = null;
         PreparedStatement ps = null;
         inscripcion = null;
-        
+
         String consulta = "Select * from inscripcion ";
-       
-            try {
-                ps = conec.prepareStatement(consulta);
-                
-                res = ps.executeQuery();
+
+        try {
+            ps = conec.prepareStatement(consulta);
+
+            res = ps.executeQuery();
             if (res != null) {
                 while (res.next()) {
                     // `idInscripto`, `idAlumno`, `idMateria`, `nota` FROM `inscripcion`
@@ -80,12 +82,15 @@ public class InscripcionData {
                     alumData = new AlumnoData();
                     mateData = new MateriaData();
 
-                    lista.add(new Inscripcion(idInscripto, alumData.buscarAlumnoID(idAlumno), mateData.buscarMateria(idMateria), nota));
+                    lista.add(new Inscripcion(idInscripto, alumData.buscarAlumnoID(idAlumno),
+                            mateData.buscarMateria(idMateria), nota));
                 }
             }
+            ps.close();
+            res.close();
 
         } catch (SQLException ex) {
-            Conexion.msjError.add("fallo la consulta: " + ex.getMessage());
+            Conexion.msjError.add("IncripcionData: obtenerInscripcion()  -> " + ex.getMessage());
         }
 
         return lista;
@@ -98,29 +103,175 @@ public class InscripcionData {
 
         inscripcion = null;
         String consulta = "Select * from inscripcion where idAlumno=?";
-       
-            try {
-                ps = conec.prepareStatement(consulta);
-                ps.setInt(1, id);
-                res = ps.executeQuery();
-                if (res.next()) {
-                    int idInscripto = res.getInt("idInscripto");
-                    int idAlumno = res.getInt("idAlumno");
-                    int idMateria = res.getInt("idMateria");
-                    double nota = res.getDouble("nota");
-                    alumData = new AlumnoData();
-                    mateData = new MateriaData();
 
-                    lista.add(new Inscripcion(idInscripto, alumData.buscarAlumnoID(idAlumno), mateData.buscarMateria(idMateria), nota));
+        try {
+            ps = conec.prepareStatement(consulta);
+            ps.setInt(1, id);
+            res = ps.executeQuery();
+            if (res.next()) {
+                int idInscripto = res.getInt("idInscripto");
+                int idAlumno = res.getInt("idAlumno");
+                int idMateria = res.getInt("idMateria");
+                double nota = res.getDouble("nota");
+                alumData = new AlumnoData();
+                mateData = new MateriaData();
 
-                }
+                lista.add(new Inscripcion(idInscripto, alumData.buscarAlumnoID(idAlumno),
+                        mateData.buscarMateria(idMateria), nota));
 
-            } catch (SQLException ex) {
-                Conexion.msjError.add("fallo la consulta: " + ex.getMessage());
             }
-        
+            ps.close();
+            res.close();
+
+        } catch (SQLException ex) {
+            Conexion.msjError.add("IncripcionData: obtenerInscripcionPorAlumno(int id) ->" + ex.getMessage());
+        }
+
         return lista;
     }
 
-    //fin
+    public ArrayList<Materia> obtenerMateriasCursadas(int idAlumno) {
+        ArrayList<Materia> lista = new ArrayList<>();
+        ResultSet res = null;
+        PreparedStatement ps = null;
+
+        inscripcion = null;
+        String consulta = "SELECT M.idMateria, nombre, año, estado FROM inscripcion I,materia M WHERE M.idMateria=I.idMateria and  idAlumno=?";
+
+        try {
+            ps = conec.prepareStatement(consulta);
+            ps.setInt(1, idAlumno);
+            res = ps.executeQuery();
+            if (res.next()) {
+                int idMateria = res.getInt("idMateria");
+                String nombre = res.getString("nombre");
+                int year = res.getInt("año");
+                boolean estado = res.getBoolean("estado");
+
+                lista.add(new Materia(idMateria, nombre, year, estado));
+
+            }
+            ps.close();
+            res.close();
+
+        } catch (SQLException ex) {
+            Conexion.msjError.add("IncripcionData:  obtenerMateriasCursadas(int idAlumno) ->" + ex.getMessage());
+        }
+
+        return lista;
+    }
+
+    public ArrayList<Materia> obtenerMateriasNoCursadas() {
+        ArrayList<Materia> lista = new ArrayList<>();
+        ResultSet res = null;
+        PreparedStatement ps = null;
+
+        inscripcion = null;
+        String consulta = "select * from materia where idMateria NOT IN (SELECT M.idMateria FROM materia M,inscripcion I WHERE M.idMateria=I.idMateria and estado=1) and estado=1;";
+        try {
+            ps = conec.prepareStatement(consulta);
+
+            res = ps.executeQuery();
+            if (res.next()) {
+                int idMateria = res.getInt("idMateria");
+                String nombre = res.getString("nombre");
+                int year = res.getInt("año");
+                boolean estado = res.getBoolean("estado");
+
+                lista.add(new Materia(idMateria, nombre, year, estado));
+
+            }
+            ps.close();
+            res.close();
+
+        } catch (SQLException ex) {
+            Conexion.msjError.add("IncripcionData:  obtenerMateriasNoCursadas() ->" + ex.getMessage());
+        }
+
+        return lista;
+    }
+
+    public boolean borrarInscripcionMateriaALumno(int idAlumno, int idMateria) {
+        String query = "DELETE FROM inscripcion WHERE idAlumno=? and idMateria=?";
+
+        boolean res = false;
+
+        try {
+            PreparedStatement ps = conec.prepareStatement(query);
+            ps.setInt(1, idAlumno);
+            ps.setInt(2, idMateria);
+            int borrado = ps.executeUpdate();
+
+            if (borrado == 1) {
+                res = true;
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+
+            Conexion.msjError.add("IncripcionData: borrarInscripcionMateriaALumno() ->" + ex.getMessage());
+        }
+
+        return res;
+    }
+
+    public boolean actualizarNota(int idAlumno, int idMateria, double nota) {
+        String query = "UPDATE inscripcion SET nota=? WHERE idAlumno=? and idMateria=?";
+
+        boolean res = false;
+
+        try {
+            PreparedStatement ps = conec.prepareStatement(query);
+            ps.setDouble(1, nota);
+            ps.setInt(2, idAlumno);
+            ps.setInt(3, idMateria);
+            int actualizar = ps.executeUpdate();
+
+            if (actualizar == 1) {
+                res = true;
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+
+            Conexion.msjError.add("MateriaData: actualizarNota() ->" + ex.getMessage());
+        }
+
+        return res;
+    }
+
+    public ArrayList<Alumno> obtenerAlumnoXMAteria(int idMateria) {
+        ArrayList<Alumno> lista = new ArrayList<>();
+        ResultSet res = null;
+        PreparedStatement ps = null;
+
+        inscripcion = null;
+        String consulta = "SELECT A.idAlumno,dni,apellido, nombre, fechaNacimiento, estado FROM inscripcion I,alumno A WHERE A.idAlumno=I.idAlumno and  idMateria=?";
+
+        try {
+            ps = conec.prepareStatement(consulta);
+            ps.setInt(1, idMateria);
+            res = ps.executeQuery();
+            if (res.next()) {
+                int idAlumno = res.getInt("idMateria");
+                int dni = res.getInt("dni");
+                String apellido = res.getString("apellido");
+                String nombre = res.getString("nombre");
+                LocalDate fechaNacimiento = res.getDate("fechaNacimiento").toLocalDate();
+                boolean estado = res.getBoolean("estado");
+
+                lista.add(new Alumno(idAlumno, dni, apellido, nombre, fechaNacimiento, estado));
+
+            }
+            ps.close();
+            res.close();
+
+        } catch (SQLException ex) {
+            Conexion.msjError.add("IncripcionData:  obtenerAlumnoXMAteria(int idMateria) ->" + ex.getMessage());
+        }
+
+        return lista;
+    }
+
+    // // fin
 }
--- src/accesoADatos/MateriaData.java
+++ src/accesoADatos/MateriaData.java
@@ -17,119 +17,121 @@ import javax.swing.JOptionPane;
  */
 public class MateriaData {
 
-    private  Materia materia;
+    private Materia materia;
     private static Connection conec = null;
 
     public MateriaData() {
         conec = Conexion.getConexion();
     }
 
-    public  boolean guardarMateria(Materia materia) {
+    public boolean guardarMateria(Materia materia) {
         String query = "INSERT INTO materia (nombre, año, estado) VALUES (?,?,?)";
 
         boolean res = false;
-       
-            try {
-                PreparedStatement ps = conec.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 
-                ps.setString(1, materia.getNombre());
-                ps.setInt(2, materia.getYear());
-                ps.setBoolean(3, materia.getEstado());
-                ps.executeUpdate();
-                ResultSet rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    materia.setIdMateria(rs.getInt(1));
-                    res = true;
-                }
-                ps.close();
-            } catch (SQLException ex) {
+        try {
+            PreparedStatement ps = conec.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 
-                Conexion.msjError.add("MAteriaData: guardarMAteria ->" + ex.getMessage());
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getYear());
+            ps.setBoolean(3, materia.getEstado());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                materia.setIdMateria(rs.getInt(1));
+                res = true;
             }
-        
-        //ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce cierra un jFrame interno
+            ps.close();
+        } catch (SQLException ex) {
+
+            Conexion.msjError.add("MAteriaData: guardarMAteria ->" + ex.getMessage());
+        }
+
+        // ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce
+        // cierra un jFrame interno
 
         return res;
     }
 
-    public  Materia buscarMateria(int id) {
+    public Materia buscarMateria(int id) {
         materia = null;
         PreparedStatement ps = null;
         String consulta = "SELECT nombre,año,estado FROM materia WHERE  idMateria= ? and estado=1";
-       
-            try {
-                ps = conec.prepareStatement(consulta);
-                ps.setInt(1, id);
-                ResultSet res = ps.executeQuery();
-                if (res.next()) {
-                    materia = new Materia();
-                    materia.setIdMateria(id);
-                    materia.setNombre(res.getString("nombre"));
-                    materia.setYear(res.getInt("año"));
-                    materia.setEstado(res.getBoolean("estado"));
-                } else {
-                    JOptionPane.showMessageDialog(null, "No existe el materia");
-                }
 
-            } catch (SQLException ex) {
-                Conexion.msjError.add("MateriaData: BuscarMteria ->" + ex.getMessage());
+        try {
+            ps = conec.prepareStatement(consulta);
+            ps.setInt(1, id);
+            ResultSet res = ps.executeQuery();
+            if (res.next()) {
+                materia = new Materia();
+                materia.setIdMateria(id);
+                materia.setNombre(res.getString("nombre"));
+                materia.setYear(res.getInt("año"));
+                materia.setEstado(res.getBoolean("estado"));
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe el materia");
             }
-        
+
+        } catch (SQLException ex) {
+            Conexion.msjError.add("MateriaData: BuscarMteria ->" + ex.getMessage());
+        }
 
         return materia;
     }
 
-    public  boolean modificarMateria(Materia materia) {
+    public boolean modificarMateria(Materia materia) {
         String query = "UPDATE materia SET nombre=?,año=?,estado=? WHERE idMateria=?";
 
         boolean res = false;
-       
-            try {
-                PreparedStatement ps = conec.prepareStatement(query);
 
-                ps.setString(1, materia.getNombre());
-                ps.setInt(2, materia.getYear());
-                ps.setBoolean(3, materia.getEstado());
-                ps.setInt(4, materia.getIdMateria());
-                ps.executeUpdate();//devuelve 1 0 mas si varias filas se afectaron y si hay erroe sale por la excepcion
-                
-                res=true;
-                ps.close();
-            } catch (SQLException ex) {
+        try {
+            PreparedStatement ps = conec.prepareStatement(query);
 
-                Conexion.msjError.add("MateriaData: modiicarAlumno() ->" + ex.getMessage());
-            }
-        
-        //ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce cierra un jFrame interno
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getYear());
+            ps.setBoolean(3, materia.getEstado());
+            ps.setInt(4, materia.getIdMateria());
+            ps.executeUpdate();// devuelve 1 0 mas si varias filas se afectaron y si hay erroe sale por la
+                               // excepcion
+
+            res = true;
+            ps.close();
+        } catch (SQLException ex) {
+
+            Conexion.msjError.add("MateriaData: modiicarAlumno() ->" + ex.getMessage());
+        }
+
+        // ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce
+        // cierra un jFrame interno
 
         return res;
     }
-    //baja logica
-    public  boolean eliminarMateria(int idMateria) {
-        String query = "UPDATE  materia SET estado=0 WHERE idMateria=?"; //la otra opcion seria darle de baja
+
+    // baja logica
+    public boolean eliminarMateria(int idMateria) {
+        String query = "UPDATE  materia SET estado=0 WHERE idMateria=?";
 
         boolean res = false;
-        
-            try {
-                PreparedStatement ps = conec.prepareStatement(query);
-                ps.setInt(1,idMateria);
-               int borrado= ps.executeUpdate();//devuelve 1 0 mas si varias filas se afectaron y si hay erroe sale por la excepcion
-                if(borrado==1){
-                    res=true;
-                }
-                ps.close();
-            } catch (SQLException ex) {
 
-                Conexion.msjError.add("MateriaData: borrado de la materia ->" + ex.getMessage());
+        try {
+            PreparedStatement ps = conec.prepareStatement(query);
+            ps.setInt(1, idMateria);
+            int borrado = ps.executeUpdate();// devuelve 1 0 mas si varias filas se afectaron y si hay erroe sale por la
+                                             // excepcion
+            if (borrado == 1) {
+                res = true;
             }
-        
-        //ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce cierra un jFrame interno
+            ps.close();
+        } catch (SQLException ex) {
+
+            Conexion.msjError.add("MateriaData: borrado de la materia ->" + ex.getMessage());
+        }
+
+        // ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce
+        // cierra un jFrame interno
 
         return res;
     }
-    
-    
-    
 
     public ArrayList<Materia> listarMateria() {
         ArrayList<Materia> lista = new ArrayList<>();
