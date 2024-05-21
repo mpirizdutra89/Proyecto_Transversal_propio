8d3d4efc2a92ed18e441396a00c17d5e54ab077b
Martin Piriz <mpirizdutra@ulp.edu.ar>
2024-05-21 20:19:26 -0300

    no funciona alumna data en el formulario

--- src/accesoADatos/AlumnoData.java
+++ src/accesoADatos/AlumnoData.java
@@ -1,6 +1,5 @@
 package accesoADatos;
 
-
 import entidades.Alumno;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,186 +10,174 @@ import java.sql.Date;
 import java.time.LocalDate;
 import javax.swing.JOptionPane;
 import java.sql.Connection;
+
 /**
  *
  * @author Nicolas
  */
 public class AlumnoData {
-     private static Alumno alumno;
-     private static Connection conec=null;
-    
-     public AlumnoData() {   
-        conec=Conexion.getConexion();
-     }
-     
-     
-     
-     
-     
-    public  boolean guardarAlumno(Alumno alumno) {
+
+    private static Alumno alumno;
+    private static Connection conec = null;
+
+    public AlumnoData() {
+        conec = Conexion.getConexion();
+    }
+
+    public boolean guardarAlumno(Alumno alumno) {
         String query = "INSERT INTO alumno (dni, apellido,nombre,fechaNacimiento,estado) VALUES (?,?,?,?,?)";
-        
-        boolean res = false; 
-       
-            try {
-                PreparedStatement ps = conec.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-                ps.setInt(1, alumno.getDni());
-                ps.setString(2, alumno.getApellido());
-                ps.setString(3, alumno.getNombre());
-                ps.setDate(4, Date.valueOf(alumno.getFechaNacimiento()));
-                ps.setBoolean(5, alumno.getEstado());
-                ps.executeUpdate();
-                ResultSet rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    alumno.setIdAlumno(rs.getInt(1));
-                    res = true;
-                }
-                ps.close();
-            } catch (SQLException ex) {
-                if (ex.getErrorCode() == 1062) {//es para que capturar campos unicos
-                    //igual va a seguir sliendo ya que es la libreria lo que lo imprime
-                    //[ WARN] (main) Error: 1062-23000: Duplicate entry '565' for key 'alumno.dni'
-                    //igual el usuario no va averla consola
-                  
-                     Conexion.msjError.add("AlumnosDAta: guardarAlumno 'Clave repetida para dni' ->" + ex.getMessage());
-                }else{
-                     Conexion.msjError.add("AlumnosDAta: guardarAlumno ->" + ex.getMessage());
-                }
-               
+
+        boolean res = false;
+
+        try {
+            PreparedStatement ps = conec.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNacimiento()));
+            ps.setBoolean(5, alumno.getEstado());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                alumno.setIdAlumno(rs.getInt(1));
+                res = true;
             }
-        
+            ps.close();
+        } catch (SQLException ex) {
+            if (ex.getErrorCode() == 1062) {//es para que capturar campos unicos
+                //igual va a seguir sliendo ya que es la libreria lo que lo imprime
+                //[ WARN] (main) Error: 1062-23000: Duplicate entry '565' for key 'alumno.dni'
+                //igual el usuario no va averla consola
+
+                Conexion.msjError.add("AlumnosDAta: guardarAlumno 'Clave repetida para dni' ->" + ex.getMessage());
+            } else {
+                Conexion.msjError.add("AlumnosDAta: guardarAlumno ->" + ex.getMessage());
+            }
+
+        }
+
         //ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce cierra un jFrame interno
-        
         return res;
     }
 
-    public  Alumno buscarAlumnoID(int id){
-        alumno=null;
+    public Alumno buscarAlumnoID(int id) {
+        alumno = null;
         PreparedStatement ps = null;
-        String consulta="SELECT dni,apellido,nombre,fechaNacimiento FROM alumno WHERE  idAlumno= ? and estado=1";
-       
-        try{
-             ps = conec.prepareStatement(consulta);
-             ps.setInt(1,id );
-             ResultSet res = ps.executeQuery();
-             if(res.next()){
-                 alumno=new Alumno();
-                 alumno.setIdAlumno(id);
-                 alumno.setDni(res.getInt("dni"));
-                 alumno.setApellido(res.getString("apellido"));
-                 alumno.setNombre(res.getString("nombre"));
-                 alumno.setFechaNacimiento(res.getDate("fechaNacimiento").toLocalDate());
-                 alumno.setEstado(res.getBoolean("estado"));
-             }
-             else{
-                 JOptionPane.showMessageDialog(null, "No existe el alumno");   
-             }
-                
-        
-         } catch (SQLException ex) {
-                Conexion.msjError.add("AlumnosDAta: BuscarAlumnoID ->" + ex.getMessage());
+        String consulta = "SELECT dni,apellido,nombre,fechaNacimiento FROM alumno WHERE  idAlumno= ? and estado=1";
+
+        try {
+            ps = conec.prepareStatement(consulta);
+            ps.setInt(1, id);
+            ResultSet res = ps.executeQuery();
+            if (res.next()) {
+                alumno = new Alumno();
+                alumno.setIdAlumno(id);
+                alumno.setDni(res.getInt("dni"));
+                alumno.setApellido(res.getString("apellido"));
+                alumno.setNombre(res.getString("nombre"));
+                alumno.setFechaNacimiento(res.getDate("fechaNacimiento").toLocalDate());
+                alumno.setEstado(res.getBoolean("estado"));
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe el alumno");
             }
-        
-            
-        
-        
+
+        } catch (SQLException ex) {
+            Conexion.msjError.add("AlumnosDAta: BuscarAlumnoID ->" + ex.getMessage());
+        }
+
         return alumno;
     }
-    
-        public  Alumno buscarAlumnoDni(int dni){
-        alumno=null;
+
+    public Alumno buscarAlumnoDni(int dni) {
+        alumno = null;
         PreparedStatement ps = null;
-        String consulta="SELECT dni,apellido,nombre,fechaNacimiento FROM alumno WHERE  dni= ? and estado=1";
-       
-        try{
-             ps = conec.prepareStatement(consulta);
-             ps.setInt(1,dni );
-             ResultSet res = ps.executeQuery();
-             if(res.next()){
-                 alumno=new Alumno();
-                 alumno.setIdAlumno(res.getInt("idAlumno"));
-                 alumno.setDni(res.getInt("dni"));
-                 alumno.setApellido(res.getString("apellido"));
-                 alumno.setNombre(res.getString("nombre"));
-                 alumno.setFechaNacimiento(res.getDate("fechaNacimiento").toLocalDate());
-                 alumno.setEstado(res.getBoolean("estado"));
-             }
-             else{
-                 JOptionPane.showMessageDialog(null, "No existe el alumno");   
-             }
+        String consulta = "SELECT idAlumno,dni,apellido,nombre, fechaNacimiento,estado  FROM alumno WHERE  dni= ? and estado=1";
+
+        try {
+            ps = conec.prepareStatement(consulta);
+            ps.setInt(1, dni);
+            ResultSet res = ps.executeQuery();
+            if (res.next()) {
                 
-        
-         } catch (SQLException ex) {
-                Conexion.msjError.add("AlumnosData: BuscarAlumnoDNI ->" + ex.getMessage());
+               
+                alumno = new Alumno();
+                alumno.setIdAlumno(res.getInt("idAlumno"));
+                alumno.setDni(res.getInt("dni"));
+                alumno.setApellido(res.getString("apellido"));
+                alumno.setNombre(res.getString("nombre"));
+             
+                alumno.setFechaNacimiento(res.getDate("fechaNacimiento").toLocalDate());
+                
+                alumno.setEstado(res.getBoolean("estado"));
+                
             }
-        
-            
-        
-        
+
+        } catch (SQLException ex) {
+            Conexion.msjError.add("AlumnosData: BuscarAlumnoDNI ->" + ex.getMessage());
+        }
+
         return alumno;
     }
-    
-     public  boolean modificarMateria(Alumno alumno) {
+
+    public boolean modificarMateria(Alumno alumno) {
         String query = "UPDATE alumno SET dni=?,apellido=?,nombre=?,fechaNacimiento=?,estado=? where idAlumno=?";
 
         boolean res = false;
-       
-            try {
-                PreparedStatement ps = conec.prepareStatement(query);
 
-                ps.setInt(1, alumno.getDni());
-                ps.setString(2, alumno.getApellido());
-                ps.setString(3, alumno.getNombre());
-                ps.setDate(4, Date.valueOf(alumno.getFechaNacimiento()));
-                ps.setBoolean(5,alumno.getEstado());
-                ps.setInt(6, alumno.getIdAlumno());
-                ps.executeUpdate();//devuelve 1 0 mas si varias filas se afectaron y si hay erroe sale por la excepcion
-                
-                res=true;
-                ps.close();
-            } catch (SQLException ex) {
+        try {
+            PreparedStatement ps = conec.prepareStatement(query);
 
-                Conexion.msjError.add("AlumnoDAta: modiicarAlumno() ->" + ex.getMessage());
-            }
-        
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNacimiento()));
+            ps.setBoolean(5, alumno.getEstado());
+            ps.setInt(6, alumno.getIdAlumno());
+            ps.executeUpdate();//devuelve 1 0 mas si varias filas se afectaron y si hay erroe sale por la excepcion
+
+            res = true;
+            ps.close();
+        } catch (SQLException ex) {
+
+            Conexion.msjError.add("AlumnoDAta: modiicarAlumno() ->" + ex.getMessage());
+        }
+
         //ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce cierra un jFrame interno
-
         return res;
     }
-    
-    public  boolean eliminarAlumno(int idAlumno) {
+
+    public boolean eliminarAlumno(int idAlumno) {
         String query = "UPDATE alumno SET estado=0 where idAlumno=?"; //la otra opcion seria darle de baja
 
         boolean res = false;
-       
-            try {
-                PreparedStatement ps = conec.prepareStatement(query);
-                ps.setInt(1,idAlumno);
-               int borrado= ps.executeUpdate();//devuelve 1 0 mas si varias filas se afectaron y si hay erroe sale por la excepcion
-                if(borrado==1){
-                    res=true;
-                }
-                ps.close();
-            } catch (SQLException ex) {
 
-                Conexion.msjError.add("AlumnosData: borrado de la alumno ->" + ex.getMessage());
+        try {
+            PreparedStatement ps = conec.prepareStatement(query);
+            ps.setInt(1, idAlumno);
+            int borrado = ps.executeUpdate();//devuelve 1 0 mas si varias filas se afectaron y si hay erroe sale por la excepcion
+            if (borrado == 1) {
+                res = true;
             }
-        
-        //ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce cierra un jFrame interno
+            ps.close();
+        } catch (SQLException ex) {
 
+            Conexion.msjError.add("AlumnosData: borrado de la alumno ->" + ex.getMessage());
+        }
+
+        //ver donde cerrrar la coneccion puede ser en el main o vista o cuando ce cierra un jFrame interno
         return res;
     }
-        
-    public  ArrayList<Alumno> listarAlumnos() {
+
+    public ArrayList<Alumno> listarAlumnos() {
         ArrayList<Alumno> lista = new ArrayList<>();
         ResultSet res = null;
-        PreparedStatement ps=null;
+        PreparedStatement ps = null;
         alumno = null;
-         String query = "select * from alumno where estado=1;";
+        String query = "select * from alumno where estado=1;";
         try {
 
             ps = conec.prepareStatement(query);
-            res=ps.executeQuery();
+            res = ps.executeQuery();
             if (res != null) {
                 while (res.next()) {
                     //`idAlumno`, `dni`, `apellido`, `nombre`, `fechaNacimiento`, `estado`
--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -14,9 +14,18 @@ public class Main {
 
     /**
      * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-      
-    }
+//     */
+//    public static void main(String[] args) {
+//        Alumno alumno=new Alumno();
+//        AlumnoData al=new AlumnoData();
+//        alumno= al.buscarAlumnoDni(34877112);
+//        if(alumno!=null){
+//             System.out.println("Valor:"+alumno.getEstado());
+//        }
+//        
+//        
+//        Conexion.mostrarErrores();
+//       
+//    }
     
 }
